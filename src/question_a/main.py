e61e660fdd2a4b4e2955bb6f135b3ed7c84c421a
jbr1ceno <143046486+jbr1ceno@users.noreply.github.com>
2023-12-08 22:54:33 +0000

    Question 1 Final

--- src/question_a/main.py
+++ src/question_a/main.py
@@ -1,1 +1,28 @@
-#add import
+import question_a
+
+def menu():
+    print("Enter 1 to make a Multiplication Table")
+    print("Enter 2 to quit program ")
+
+
+menu()
+option = int(input("Please enter a number: "))
+
+
+while option != 2:
+    if(option == 1):
+        num1 = input("Enter the operand: ")
+        num2 = 10
+        result = question_a.display_multiplication_table(question_a.create_multiplication_table(int(num1),int(num2)))
+        #print("The multiplication table for ", num1, "is: ", result)
+        exitProgram = input("If you want to quit, type yes. If not type something else: ")
+        if(exitProgram.upper() == "YES"):
+            break
+    else:
+        print("Value not valid.")
+
+    print()
+    menu()
+    option = int(input("Enter your option: "))
+
+print("You have terminated the program. ")
--- src/question_a/question_a.py
+++ src/question_a/question_a.py
@@ -1,3 +1,23 @@
 #write functions here, don't add input('') statements here!
-def test_config():
-    return True
+
+
+def create_multiplication_table(max_valueR, max_valueC):
+    m_table = []
+
+    for r in range(1, max_valueR + 1):
+        row = []
+
+        for c in range(1, max_valueC + 1):
+            row.append(r*c)
+
+        m_table.append(row)
+
+    return m_table
+
+def display_multiplication_table(m_table):
+
+    #stringEmpty = " "
+    #print(m_table)
+    for r in range(len(m_table)):
+        #for c in range(len(m_table[r])):
+            print(*m_table[r], " ")
