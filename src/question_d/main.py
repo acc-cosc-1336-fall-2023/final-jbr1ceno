ccaa197c7c42c02b6378686a4c3d1665730a6b2d
jbr1ceno <143046486+jbr1ceno@users.noreply.github.com>
2023-12-10 01:40:26 +0000

    Question 4 Final (WIP)

--- src/question_d/main.py
+++ src/question_d/main.py
@@ -1,1 +1,27 @@
-#add import
+from question_d import Stock
+
+stockListView = Stock("MSFT", "Microsoft")
+
+stockListView.get_stock_list()
+
+'''def menu():
+    print("1- Display stock purchase history ")
+    print("2- Exit ")
+
+menu()
+option = int(input("Please enter a number: "))
+
+while option != 2:
+    if(option == 1):
+        question_d.get_stock_list('stocklist.txt', 'r')
+        exitProgram = input("If you want to quit, type yes. If not type something else: ")
+        if(exitProgram.upper() == "YES"):
+            break
+    else:
+        print("Value not valid.")
+
+    print()
+    menu()
+    option = int(input("Enter your option: "))
+
+print("You have terminated the program. ")'''
--- src/question_d/question_d.py
+++ src/question_d/question_d.py
@@ -1,1 +1,36 @@
 #write functions here, don't add input('') statements here!
+class Stock:
+
+    __file_name = 'stock_file.dat'
+
+    def __init__(self, symbol, company_name):
+
+        __symbol = symbol
+        __company_name = company_name
+
+    def get_symbol(self):
+        return self.__symbol
+    
+    def get_company_name(self):
+        return self.__company_name
+
+    def get_stock_list(self):
+        
+        in_file = open(self.__file_name, 'r')
+
+        file_contents = in_file.read()
+
+        '''while(line != ''):
+            print(file_contents)
+            file_contents = in_file.readline()'''
+
+
+        in_file.close()
+
+        print(file_contents)
+
+
+
+    '''def __init__(self, symbol, company_name): #constructor
+        self.__symbol = str(symbol)
+        self.__company_name = str(company_name)'''
